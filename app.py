3c9495e387a4f075026fef97cff86b92a2e83173
vishalcandy <bejjankisaivishal881@gmail.com>
2022-06-21 12:01:09 +0530

    donation form

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def homepage():
     return render_template('home.html')
 
+@app.route('/donationform')
+def donationform():
+    return render_template('donationform.html')
+
 if __name__ == '__main__':
   app.run(debug=True)

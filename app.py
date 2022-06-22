28ca90f54684250a078010beb5c24cf9aee1827e
vishalcandy <bejjankisaivishal881@gmail.com>
2022-06-22 12:10:06 +0530

    ...

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+
+from ast import If
+from re import A
+from flask import Flask, render_template, request,redirect
 
 app = Flask(__name__)
 
@@ -6,13 +9,48 @@ app = Flask(__name__)
 def homepage():
     return render_template('home.html')
 
-@app.route('/donationform')
+@app.route('/donationform', methods=['GET','POST'])
 def donationform():
-    return render_template('donationform.html')
+    if request.method == 'GET':
+        return render_template('donationform.html')
+    elif request.method == 'POST':
+        g=request.form['email']
+        A=request.form['number']
+        h=request.form['book name']
+        x=request.form['address']
+        book={
+            'email': g,
+            'number': A,
+            'book' : h,
+            'address': x
+        } 
+        print (book)
+        
+        return render_template('donationform.html')
+
+
+
 
 @app.route('/donationlist')
 def donationlist():
     return render_template('donationlist.html')
+
+e='admin username' 
+f='admin password'
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    elif request.method == 'POST':
+        d=request.form['username']
+        c=request.form['password']
+        if d == e and c == f:
+           return redirect('/donationlist')
+
+        else: 
+            return render_template('login.html')
+
 
 if __name__ == '__main__':
   app.run(debug=True)
